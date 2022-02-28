feeed0df2b513bac6fb40f4afce526005edd217e
josfeenstra <me@josfeenstra.nl>
2022-02-28 16:26:36 +0100

    we got a nice looking logo

--- side-projects/html/3/src/index.ts
+++ side-projects/html/3/src/index.ts
@@ -4,14 +4,27 @@ import "./html/web-components/my-counter"
 import "./html/web-components/geofront-logo"
 import "./html/web-components/x-styler"
 
+function getVersion() {
+    return "0.0.3";
+}
+
 function main() {
     
     document.body.append(Div.html`
-        <geofront-logo></geofront-logo><h1>Geofront Version ${100}</h1>
+
+        <div style="display: flex; align-items: center">
+            <geofront-logo></geofront-logo>
+            <div style="align-items: middle">
+                <h6 style="padding: 0px; margin: 0px; color: darkgrey">Version ${getVersion()}</h6>
+                <h1 style="padding: 0px; margin: 0px">Geofront</h1>
+            </div>
+        </div>
         <fast-menu-wrapper></fast-menu-wrapper>
         <progress-bar percent="90"></progress-bar>
         <my-counter></my-counter>
         <x-styler>Text</x-styler>
+
+
     `);
 }
 
